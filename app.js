c12d06286478ac0142cde8e9bc92783bab13ee41
unknown <martrell3030@gmail.com>
2018-10-07 14:49:09 -0700

    add files

---
+++ app.js
@@ -0,0 +1,18 @@
+
+new Vue({
+  el: '#vue-app',
+  data: {
+    students: ['Terry', 'Lisa', 'Mark', 'Jenny'],
+    aStudents: [
+      {name: 'Ken', grade: 95},
+      {name: 'Paul', grade: 92},
+      {name: 'Tiffany', grade: 96}
+    ]
+  },
+  methods: {
+
+  },
+  computed: {
+
+  }
+});
