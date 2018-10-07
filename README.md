0692b344c173ee8f9b163319144f58ddf69377ce
unknown <martrell3030@gmail.com>
2018-10-07 16:53:29 -0700

    add all files

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vue (v-for) loop
--- app.js
+++ app.js
@@ -2,15 +2,20 @@
 new Vue({
   el: '#vue-app',
   data: {
-    students: ['Terry', 'Lisa', 'Mark', 'Jenny'],
-    aStudents: [
-      {name: 'Ken', grade: 95},
-      {name: 'Paul', grade: 92},
-      {name: 'Tiffany', grade: 96}
-    ]
+    health: 100,
+    ended: false
   },
   methods: {
-
+    punch: function() {
+      this.health -= 10;
+      if(this.health <= 0) {
+        this.ended = true;
+      }
+    },
+    restart: function() {
+      this.health = 100;
+      this.ended = false;
+    }
   },
   computed: {
 
